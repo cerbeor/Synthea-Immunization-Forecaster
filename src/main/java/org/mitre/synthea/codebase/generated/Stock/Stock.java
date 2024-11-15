d22f490ad0dcd0752cdbf9dabcf5eb4a19b8806f
David <daorlx541@gmail.com>
2024-11-15 16:09:38 +0100

    Update Stock

---
+++ src/main/java/org/mitre/synthea/codebase/generated/Stock/Stock.java
@@ -0,0 +1,12 @@
+package org.mitre.synthea.codebase.generated.Stock;
+
+import java.util.HashMap;
+
+public class Stock {
+
+    HashMap <String, Boolean> mapStock;
+
+    // Fonction qui créer la map stock à partir de la liste des NDC existant
+    // en attributant aléatoirement true ou false
+    
+}
