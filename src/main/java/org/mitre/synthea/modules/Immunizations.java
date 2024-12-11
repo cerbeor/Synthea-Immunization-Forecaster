44dca05d207554bb4e4778689b43e6adcac7972b
ZHEN Julie <julie.zhen@telecomnancy.eu>
2024-12-11 14:17:19 +0100

    comment logs

--- src/main/java/org/mitre/synthea/modules/Immunizations.java
+++ src/main/java/org/mitre/synthea/modules/Immunizations.java
@@ -316,6 +316,7 @@ public class Immunizations {
           // Get all cvx relating to the NDC
         for (org.mitre.synthea.codebase.generated.Code cvx : ndc.getCvxCodes()) {
             cvxMap.put(cvx, ndc);
+          System.out.println(" CVX code : " + cvx.getValue() + " CVX name : " + cvx.getLabel() + " NDC code : " + ndc.getNdcCode());
         }
 
       }
